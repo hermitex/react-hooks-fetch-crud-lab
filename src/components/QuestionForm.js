8e40923c3e324fa8879b541747e078e3beb46018
hermitex <mercytibs@yahoo.com>
2022-07-21 19:12:29 +0300

    Done.

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,17 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+
+    let data = {
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3],
+      correctIndex: formData.correctIndex,
+    };
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(data),
+    });
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
+
+  function onDeleteQuestion(deletedQuestion){
+    let newQuestions = questions.filter(question => question.id !== deletedQuestion.id);
+    setQuestions(newQuestions);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} onDeleteQuestion={onDeleteQuestion} />
+        ))}
+      </ul>
     </section>
   );
 }
